2fde4300f3ca976acd99b4bed5b6b068f29a5b67
sstefdev <sstefandev@gmail.com>
2024-05-08 00:08:50 +0200

    feat: added shared web-component

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -5,26 +5,26 @@ export {
   type InvoiceItem,
   type CustomFormData,
   type SellerBuyerInfo,
-} from "./types";
+} from "./lib/types";
 
-export { APP_STATUS } from "./types/enums";
+export { APP_STATUS } from "./lib/types/enums";
 
 // Components
-export { default as Copy } from "./ui/copy.svelte";
-export { default as Input } from "./ui/input.svelte";
-export { default as Modal } from "./ui/modal.svelte";
-export { default as Labels } from "./ui/labels.svelte";
-export { default as Button } from "./ui/button.svelte";
-export { default as Status } from "./ui/status.svelte";
-export { default as Skeleton } from "./ui/skeleton.svelte";
-export { default as Dropdown } from "./ui/dropdown.svelte";
-export { default as Accordion } from "./ui/accordion.svelte";
+export { default as Copy } from "./lib/ui/copy.svelte";
+export { default as Input } from "./lib/ui/input.svelte";
+export { default as Modal } from "./lib/ui/modal.svelte";
+export { default as Labels } from "./lib/ui/labels.svelte";
+export { default as Button } from "./lib/ui/button.svelte";
+export { default as Status } from "./lib/ui/status.svelte";
+export { default as Skeleton } from "./lib/ui/skeleton.svelte";
+export { default as Dropdown } from "./lib/ui/dropdown.svelte";
+export { default as Accordion } from "./lib/ui/accordion.svelte";
 
 // Utils
-export { config } from "./utils/config";
-export { currencies } from "./utils/currencies";
-export { formatDate, inputDateFormat } from "./utils/formatDate";
+export { config } from "./lib/utils/config";
+export { currencies } from "./lib/utils/currencies";
+export { formatDate, inputDateFormat } from "./lib/utils/formatDate";
 export {
   calculateItemTotal,
   calculateInvoiceTotals,
-} from "./utils/invoiceTotals";
+} from "./lib/utils/invoiceTotals";
---
+++ packages/shared/src/lib/index.ts
@@ -0,0 +1,30 @@
+// Types
+export {
+  type IConfig,
+  type Address,
+  type InvoiceItem,
+  type CustomFormData,
+  type SellerBuyerInfo,
+} from "./types";
+
+export { APP_STATUS } from "./types/enums";
+
+// Components
+export { default as Copy } from "./ui/copy.svelte";
+export { default as Input } from "./ui/input.svelte";
+export { default as Modal } from "./ui/modal.svelte";
+export { default as Labels } from "./ui/labels.svelte";
+export { default as Button } from "./ui/button.svelte";
+export { default as Status } from "./ui/status.svelte";
+export { default as Skeleton } from "./ui/skeleton.svelte";
+export { default as Dropdown } from "./ui/dropdown.svelte";
+export { default as Accordion } from "./ui/accordion.svelte";
+
+// Utils
+export { config } from "./utils/config";
+export { currencies } from "./utils/currencies";
+export { formatDate, inputDateFormat } from "./utils/formatDate";
+export {
+  calculateItemTotal,
+  calculateInvoiceTotals,
+} from "./utils/invoiceTotals";
