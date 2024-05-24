6996480cbfadf7d9d8b12270d2e7c865ad09dd79
MantisClone <david.hunt-mateo@mantisdata.solutions>
2024-05-24 11:22:59 -0400

    Change default builderId and reorder config properties

--- packages/shared/src/lib/utils/config.ts
+++ packages/shared/src/lib/utils/config.ts
@@ -2,11 +2,11 @@ import logo from "../assets/logo-sm.svg";
 import { IConfig } from "../types";
 
 export const config: IConfig = {
-  logo,
-  builderId: "",
+  builderId: "your-project-name",
   dashboardLink: "/",
   colors: {
     main: "#0BB489",
     secondary: "#58E1A5",
   },
+  logo,
 };
