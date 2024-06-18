73768cecd853646cb62e9c35d924f6041b911369
sstefdev <sstefandev@gmail.com>
2024-06-18 20:42:39 +0200

    fix: updated npm-publish and is-release script

--- scripts/is-release-needed.sh
+++ scripts/is-release-needed.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-# This script checks if the current version of the package is already published on npm
-PACKAGE_VERSION="$(node -p -e "require('./package.json').version")"
-PACKAGE_NAME="$(node -p -e "require('./package.json').name")"
+# This script takes the package directory as an argument
+PACKAGE_DIR=$1
+
+# Change to the specific package directory
+cd $PACKAGE_DIR
+
+# Check if the current version of the package is already published on npm
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
+PACKAGE_NAME=$(node -p "require('./package.json').name")
+
+echo "Checking if $PACKAGE_NAME@$PACKAGE_VERSION is already published..."
 
 FOUND_VERSION=$(npm view $PACKAGE_NAME versions --json | grep -o "\"$PACKAGE_VERSION\"")
 
@@ -12,5 +20,4 @@ else
     IS_NEW_VERSION=false
 fi
 
-echo "is-release-needed=$IS_NEW_VERSION" >> $GITHUB_ENV
-
+echo "is-release-needed=$IS_NEW_VERSION" >> $GITHUB_OUTPUT
