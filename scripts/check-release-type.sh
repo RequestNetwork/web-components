c235bff5674309e008f40dfd28acca141a4ecf28
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-11-04 15:16:19 +0300

    feat: add `--next` releases to the github action (#179)

---
+++ scripts/check-release-type.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script checks if we need a stable or next release
+PACKAGE_VERSION="$(node -p -e "require('./package.json').version")"
+PACKAGE_NAME="$(node -p -e "require('./package.json').name")"
+
+# Check if current version exists in npm
+FOUND_VERSION=$(npm view $PACKAGE_NAME versions | grep $PACKAGE_VERSION)
+
+if [ -z "$FOUND_VERSION" ]; then
+    echo 'stable' # New version in package.json, do stable release
+else
+    # Check if there are changes since last release
+    git diff --quiet HEAD~1 HEAD -- .
+    if [ $? -eq 1 ]; then
+        echo "next" # Changes detected, do next release
+    else
+        echo 'none' # No changes, no release needed
+    fi
+fi
