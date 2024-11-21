122fc289dff8ed21a034895b1d2d801311861002
sstefdev <sstefandev@gmail.com>
2024-11-21 17:19:56 +0100

    feat: added better skeleton on invoice dashboard

---
+++ shared/components/dashboard-skeleton.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Skeleton from "./skeleton.svelte";
+
+  export let rows = 7;
+  export let columns = 10;
+  let skeletonConfig = {
+    colors: {
+      main: "#D9D9D9",
+      secondary: "#E7E7E7",
+    },
+  };
+</script>
+
+{#each Array(columns) as _}
+  <tr class="skeleton-wrapper">
+    {#each Array(rows) as _}
+      <td class="skeleton-item"
+        ><Skeleton config={skeletonConfig} lineCount={1} /></td
+      >
+    {/each}
+  </tr>
+  <tr class="skeleton-item"></tr>
+{/each}
+
+<style>
+  .skeleton-item {
+    padding: 20px;
+    border-bottom: 1px solid #d1d5db;
+  }
+</style>
