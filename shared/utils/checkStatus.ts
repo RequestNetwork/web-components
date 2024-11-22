4775206d3a8fce86a970d6aeb4f70deec44d1b61
sstefdev <sstefandev@gmail.com>
2024-11-22 23:46:17 +0100

    fix: overdue and partialy paid fix

--- shared/utils/checkStatus.ts
+++ shared/utils/checkStatus.ts
@@ -4,18 +4,19 @@ import { Types } from "@requestnetwork/request-client.js";
 export const checkStatus = (request: Types.IRequestDataWithEvents | null) => {
   const balance = Number(request?.balance?.balance ?? 0);
   const expectedAmount = Number(request?.expectedAmount ?? 0);
-
-  if (balance > 0) {
-    return balance >= expectedAmount ? "Paid" : "Partially Paid";
-  }
+  const today = new Date();
+  const dueDate = new Date(request?.contentData?.paymentTerms?.dueDate);
 
   const eventStatus = {
     reject: "Rejected",
     cancel: "Canceled",
   };
 
-  const dueDate = request?.contentData?.paymentTerms?.dueDate;
-  if (dueDate && new Date(dueDate) < new Date()) {
+  if (balance > 0) {
+    return balance >= expectedAmount ? "Paid" : "Partially Paid";
+  }
+
+  if (dueDate < today && balance === 0) {
     return "Overdue";
   }
 
