8e984dee269c22a32418c4afe8e3a0784950c1c7
sstefdev <sstefandev@gmail.com>
2024-11-24 09:02:40 +0100

    fix: updated checkStatus function

--- shared/utils/checkStatus.ts
+++ shared/utils/checkStatus.ts
@@ -2,23 +2,16 @@ import { capitalize } from "./capitalize";
 import { Types } from "@requestnetwork/request-client.js";
 
 export const checkStatus = (request: Types.IRequestDataWithEvents | null) => {
-  const balance = Number(request?.balance?.balance ?? 0);
-  const expectedAmount = Number(request?.expectedAmount ?? 0);
+  const balance = BigInt(request?.balance?.balance ?? 0);
+  const expectedAmount = BigInt(request?.expectedAmount ?? 0);
   const today = new Date();
   const dueDate = new Date(request?.contentData?.paymentTerms?.dueDate);
+  const isPaid = balance >= expectedAmount ? "Paid" : "Partially Paid";
 
   const eventStatus = {
     reject: "Rejected",
     cancel: "Canceled",
   };
-
-  if (balance > 0) {
-    return balance >= expectedAmount ? "Paid" : "Partially Paid";
-  }
-
-  if (dueDate < today && balance === 0) {
-    return "Overdue";
-  }
 
   for (const [event, status] of Object.entries(eventStatus)) {
     if (
@@ -26,9 +19,19 @@ export const checkStatus = (request: Types.IRequestDataWithEvents | null) => {
         (e: { name?: string }) => e?.name?.toLowerCase() === event.toLowerCase()
       )
     ) {
-      return status;
+      return capitalize(status);
     }
   }
 
-  return capitalize(request?.state as string);
+  if (dueDate < today) {
+    if (balance === BigInt(0)) {
+      return "Overdue";
+    }
+    return isPaid;
+  } else {
+    if (balance === BigInt(0)) {
+      return "Awaiting Payment";
+    }
+    return isPaid;
+  }
 };
