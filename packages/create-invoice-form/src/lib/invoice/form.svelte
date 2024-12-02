8a9ad466a71b7fda9a7a0aab01cd00187689cc3f
sstefdev <sstefandev@gmail.com>
2024-12-02 17:05:06 +0100

    feat: added new design changes

--- packages/create-invoice-form/src/lib/invoice/form.svelte
+++ packages/create-invoice-form/src/lib/invoice/form.svelte
@@ -588,8 +588,8 @@
         max={200}
         id="note"
         {handleInput}
-        type="textarea"
-        placeholder="Memo"
+        type="text"
+        label="Memo"
         value={formData.note}
       />
       <Labels {config} bind:formData />
@@ -626,6 +626,7 @@
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.06);
     gap: 20px;
     box-sizing: border-box;
+    min-width: 700px;
   }
 
   .invoice-form-container {
@@ -792,8 +793,8 @@
 
   .invoice-form-label-wrapper {
     display: flex;
-    align-items: center;
-    gap: 16px;
+    flex-direction: column;
+    gap: 8px;
     height: fit-content;
     width: 100%;
   }
@@ -803,23 +804,9 @@
     font-size: 12px;
   }
 
-  :global(.invoice-form-label-wrapper .input-wrapper) {
-    flex: 1;
-  }
-
   :global(.invoice-form-label-wrapper svg, .invoice-form-label-wrapper path) {
     color: white;
     fill: white;
-  }
-
-  :global(.invoice-form-label-wrapper .input-wrapper .textarea-input) {
-    width: 100%;
-    height: 107px;
-  }
-
-  :global(.invoice-form-label-wrapper .labels-wrapper) {
-    flex: 1;
-    margin-right: 8px;
   }
 
   :global(.invoice-form-table-body-add-item button) {
