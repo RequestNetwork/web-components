af3e9f883791f5bae45321bcf85c6c6d6753288e
Aimen Sahnoun <aimensahnoun@outlook.com>
2025-01-16 18:59:10 +0300

    feat: add default currencies into the component (#309)

--- shared/utils/initCurrencyManager.ts
+++ shared/utils/initCurrencyManager.ts
@@ -1,67 +1,36 @@
 import { CurrencyManager } from "@requestnetwork/currency";
 import { CurrencyTypes } from "@requestnetwork/types";
+import { formattedCurrencyConversionPairs } from "./currencyConversionPairs";
 
 const defaultCurrencyIds = [
   "USD",
   "EUR",
-  "GBP",
-  "CHF",
-  "SGD",
-  "AUD",
-  "BRL",
-  "CAD",
-  "INR",
-  "JPY",
-  "KRW",
-  "IDR",
-  "NZD",
-  "TRY",
   "CNY",
-  "FAU-sepolia",
-  "USDC-mainnet",
-  "USDT-mainnet",
-  "DAI-mainnet",
-  "USDC-matic",
-  "USDT-matic",
-  "DAI-matic",
-  "USDC-matic",
-  "AXS-mainnet",
-  "AUDIO-mainnet",
-  "RAI-mainnet",
-  "SYLO-mainnet",
-  "LDO-mainnet",
-  "UST-mainnet",
-  "MNT-mainnet",
-  "MIR-mainnet",
-  "INJ-mainnet",
-  "OCEAN-mainnet",
-  "ANKR-mainnet",
-  "RLY-mainnet",
+  "GBP",
+  "JPY",
   "DAI-bsc",
   "BUSD-bsc",
-  "USDC-xdai",
-  "USDC-avalanche",
-  "USDT-avalanche",
-  "USDC-optimism",
-  "USDT-optimism",
-  "DAI-optimism",
-  "USDC-multichain-moonbeam",
-  "USDC-wormhole-moonbeam",
+  "ETH-base-base",
+  "DAI-mainnet",
   "ETH-mainnet",
   "REQ-mainnet",
+  "USDC-mainnet",
+  "USDT-mainnet",
+  "DAI-matic",
   "MATIC-matic",
-  "FTM-fantom",
-  "AVAX-avalanche",
+  "USDC-matic",
+  "USDT-matic",
+  "DAI-optimism",
   "ETH-optimism-optimism",
-  "MNT-mantle",
+  "USDC-optimism",
+  "USDT-optimism",
+  "FAU-sepolia",
   "ETH-sepolia-sepolia",
-  "ETH-zksync-zksyncera",
-  "ETH-base-base",
-  "fUSDT-sepolia",
   "fUSDC-sepolia",
+  "fUSDT-sepolia",
+  "USDC-xdai",
+  "USDC-base",
 ];
-
-import { formattedCurrencyConversionPairs } from "./currencyConversionPairs";
 
 const TOKEN_LIST_URL =
   "https://requestnetwork.github.io/request-token-list/latest.json";
@@ -106,11 +75,14 @@ export function initializeCurrencyManagerWithCurrencyIDS(
 export async function initializeCreateInvoiceCurrencyManager(
   customCurrencyIds: string[]
 ): Promise<CurrencyManager<any>> {
+  const currenciesToUse =
+    customCurrencyIds.length > 0 ? customCurrencyIds : defaultCurrencyIds;
+
   const tokens = await fetchTokenList();
 
   const tokenMap = new Map(tokens.map((token: any) => [token.id, token]));
 
-  const currencies = customCurrencyIds
+  const currencies = currenciesToUse
     .map((id) => tokenMap.get(id))
     .filter((token): token is CurrencyTypes.CurrencyInput => token != null);
 
